e922ca06c787c5cfc35b6d260dfe81df8b0cb3f9
Parvez <rocksparvezmulla1.com>
2018-11-15 23:17:59 +0530

    settin file updated

--- GoComet/settings.py
+++ GoComet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ljoylog2uomzk^@3hcbr+izmgaoa5asv#$#!=-c9^+g$8fp%($'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gocomet.herokuapp.com']
 
 
 # Application definition
