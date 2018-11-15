b87f63311f1b3ecbf58ca09c50d2320c322c7ed2
Parvez <rocksparvezmulla1.com>
2018-11-15 22:56:27 +0530

    hosting setup files added

--- GoComet/settings.py
+++ GoComet/settings.py
@@ -128,12 +128,16 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(PROJECT_ROOT, "static"),
     '/var/www/static/',
 ]
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn BankApi.wsgi
